1dbbb9fab12508b306c55cdff07f15846841d39a
SuianeHenrichs1 <suiane.henrichs@yahoo.com.br>
2025-01-29 22:45:40 -0300

    Primeiro commit 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // Incluir o arquivo HTML
+    "./src/**/*.{html,js}",  // Incluir arquivos HTML e JS dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+  
+  
+  
+  
+  
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // Diretório raiz do seu projeto (onde está o index.html)
+  build: {
+    outDir: 'dist', // Diretório de saída para os arquivos compilados
+  },
+});
+
+
+  
+
+
+
